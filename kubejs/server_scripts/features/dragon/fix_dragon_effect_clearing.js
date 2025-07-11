a83a8fc9e0c48ecfb9879f29f7ae5883f76c5f0d
JesterRomut <lxcmoriah@gmail.com>
2025-07-11 16:47:43 +0800

    修复龙食用充能煤等物品时清除自身视野buff等正面效果的问题。因底层代码原因，现在食用牛奶也不会消除那些buff了。

---
+++ kubejs/server_scripts/features/dragon/fix_dragon_effect_clearing.js
@@ -0,0 +1,42 @@
+const effectsToPreserveWithMilk = [
+    "dragonsurvival:lava_vision",
+    "dragonsurvival:animal_peace",
+    "dragonsurvival:water_vision",
+    "dragonsurvival:cave_fire",
+    "dragonsurvival:forest_magic",
+    "dragonsurvival:hunter",
+    "dragonsurvival:sea_peace",
+];
+NativeEvents.onEvent(
+    "net.neoforged.neoforge.event.entity.living.MobEffectEvent$Applicable",
+    (
+        /**@type {import("net.neoforged.neoforge.event.entity.living.MobEffectEvent$Applicable").$MobEffectEvent$Applicable} */ event
+    ) => {
+        const $EffectCures = Java.loadClass(
+            "net.neoforged.neoforge.common.EffectCures"
+        );
+
+        const { effectInstance } = event;
+        if (!effectsToPreserveWithMilk.some((id) => effectInstance.is(id)))
+            return;
+        effectInstance.cures.remove($EffectCures.MILK);
+    }
+);
+
+// global.Example$MobEffectEvent$Applicable = (
+//     /**@type {Internal.MobEffectEvent$Applicable} */ event
+// ) => {
+//     const $EffectInstance = Java.loadClass(
+//         "net.minecraft.world.effect.MobEffectInstance"
+//     );
+//     const { effectInstance } = event;
+//     const curativeItems = event.effectInstance.curativeItems;
+//     if (
+//         effectsToClearWithMilk.some((effect) =>
+//             new $EffectInstance(effect).effect.equals(effectInstance.effect)
+//         )
+//     )
+//         return;
+//     curativeItems.removeIf((item) => item.hasTag("forge:milk/milk"));
+//     effectInstance.setCurativeItems(curativeItems);
+// };
